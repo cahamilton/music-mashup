4493a3cd593f425ff7e864723ed036d4a08d7a11
Carl Hamilton <carlanthonyhamilton@gmail.com>
2016-01-12 01:19:23 +1100

    Added search API method
    - Added functionality to search for artist by name.
    - Method will return array of artist objects containing: 'name', 'mbid' (MusicBrainz ID) and 'thumbnail' urls.

--- routes/api.js
+++ routes/api.js
@@ -3,4 +3,23 @@ var router = express.Router();
 
 var search = require('./api/search');
 
+router.get('/search/', function(req, res, next) {
+  res.status(404).send({
+    error: 'Missing artistName parameter'
+  });
+});
+
+router.get('/search/:artistName', function(req, res, next) {
+  var artistName = req.params.artistName;
+  search.artist(artistName, function(data) {
+    res.json(data);
+  });
+});
+
+router.use(function(req, res, next) {
+  res.status(404).send({
+    error: 'Invalid API Method called'
+  });
+});
+
 module.exports = router;
--- test/routes/api/search.js
+++ test/routes/api/search.js
@@ -1,9 +1,430 @@
 var chai = require('chai');
-var assert = chai.assert;
+var expect = chai.expect;
 
 var search = require('../../../routes/api/search');
 
+// Mock response data from LastFM
+/* eslint-disable */
+var data = {
+  "results": {
+    "opensearch:Query": {
+      "#text": "",
+      "role": "request",
+      "searchTerms": "offspring",
+      "startPage": "1"
+    },
+    "opensearch:totalResults": "0",
+    "opensearch:startIndex": "0",
+    "opensearch:itemsPerPage": "20",
+    "artistmatches": {
+      "artist": [{
+        "name": "The Offspring",
+        "listeners": "2966408",
+        "mbid": "23a03e33-a603-404e-bcbf-2c00159d7067",
+        "url": "http://www.last.fm/music/The+Offspring",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/58e8399fa68842fea5f8e59348a0727a.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/58e8399fa68842fea5f8e59348a0727a.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/58e8399fa68842fea5f8e59348a0727a.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/58e8399fa68842fea5f8e59348a0727a.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/58e8399fa68842fea5f8e59348a0727a.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "♪♥♪The Offspring",
+        "listeners": "667",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E2%99%AA%E2%99%A5%E2%99%AAThe+Offspring",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/e0b1830cb69f4d8acd74e1372b0ed91d.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/e0b1830cb69f4d8acd74e1372b0ed91d.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/e0b1830cb69f4d8acd74e1372b0ed91d.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/e0b1830cb69f4d8acd74e1372b0ed91d.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/e0b1830cb69f4d8acd74e1372b0ed91d.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "The Offspring & Green Day",
+        "listeners": "1021",
+        "mbid": "",
+        "url": "http://www.last.fm/music/The+Offspring+&+Green+Day",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/dcef25c0554449208386ae9ec95045a5.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/dcef25c0554449208386ae9ec95045a5.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/dcef25c0554449208386ae9ec95045a5.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/dcef25c0554449208386ae9ec95045a5.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/dcef25c0554449208386ae9ec95045a5.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "Fatboy Slim vs. Offspring",
+        "listeners": "2140",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Fatboy+Slim+vs.+Offspring",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/2d5358f3789646d58c58c8b41cba0877.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/2d5358f3789646d58c58c8b41cba0877.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/2d5358f3789646d58c58c8b41cba0877.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/2d5358f3789646d58c58c8b41cba0877.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/2d5358f3789646d58c58c8b41cba0877.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "Seeed vs. Offspring vs. Dem Franchize Boyz",
+        "listeners": "1162",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Seeed+vs.+Offspring+vs.+Dem+Franchize+Boyz",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "Fat Boy Slim VS OffSpring",
+        "listeners": "2503",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Fat+Boy+Slim+VS+OffSpring",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "Celtic Offspring",
+        "listeners": "97",
+        "mbid": "985dcdd3-3968-42c2-9057-97b1413ef824",
+        "url": "http://www.last.fm/music/Celtic+Offspring",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "The Offspring - www.galeriadamusica.net",
+        "listeners": "58",
+        "mbid": "",
+        "url": "http://www.last.fm/music/The+Offspring+-+www.galeriadamusica.net",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "Final Offspring",
+        "listeners": "187",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Final+Offspring",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/35987c559df444b3ba40f097ab7db97d.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/35987c559df444b3ba40f097ab7db97d.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/35987c559df444b3ba40f097ab7db97d.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/35987c559df444b3ba40f097ab7db97d.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/35987c559df444b3ba40f097ab7db97d.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "Bedlam's Offspring",
+        "listeners": "814",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Bedlam%27s+Offspring",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/d0f83ce0938c496ac93666e206c86f06.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/d0f83ce0938c496ac93666e206c86f06.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/d0f83ce0938c496ac93666e206c86f06.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/d0f83ce0938c496ac93666e206c86f06.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/d0f83ce0938c496ac93666e206c86f06.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "♫The Offspring [1998 - Americana]",
+        "listeners": "74",
+        "mbid": "",
+        "url": "http://www.last.fm/music/%E2%99%ABThe+Offspring+%5B1998+-+Americana%5D",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "The Offspring - www.musicgratis.org",
+        "listeners": "15",
+        "mbid": "",
+        "url": "http://www.last.fm/music/The+Offspring+-+www.musicgratis.org",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/99b5cca88938489e93999f3d7ff4c643.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/99b5cca88938489e93999f3d7ff4c643.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/99b5cca88938489e93999f3d7ff4c643.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/99b5cca88938489e93999f3d7ff4c643.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/99b5cca88938489e93999f3d7ff4c643.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "The Offspring | www.virtuarock.net",
+        "listeners": "54",
+        "mbid": "",
+        "url": "http://www.last.fm/music/The+Offspring+%7C+www.virtuarock.net",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "The Offspring - www.milsic.blogspot.com",
+        "listeners": "64",
+        "mbid": "",
+        "url": "http://www.last.fm/music/The+Offspring+-+www.milsic.blogspot.com",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "James Brown vs. The Offspring",
+        "listeners": "780",
+        "mbid": "",
+        "url": "http://www.last.fm/music/James+Brown+vs.+The+Offspring",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "Pennywise & Offspring",
+        "listeners": "228",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Pennywise+&+Offspring",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "THE OFFSPRING feat. REDMAN",
+        "listeners": "139",
+        "mbid": "23a03e33-a603-404e-bcbf-2c00159d7067",
+        "url": "http://www.last.fm/music/THE+OFFSPRING+feat.+REDMAN",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "Limp Bizkit & Rage Against The Machine & POD & Offspring & Greenday & Nirvana & Pearl Jam",
+        "listeners": "789",
+        "mbid": "",
+        "url": "http://www.last.fm/music/Limp+Bizkit+&+Rage+Against+The+Machine+&+POD+&+Offspring+&+Greenday+&+Nirvana+&+Pearl+Jam",
+        "streamable": "0",
+        "image": [{
+          "#text": "http://img2-ak.lst.fm/i/u/34s/2bf5dc4212d445d0b7359439f359105e.png",
+          "size": "small"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/64s/2bf5dc4212d445d0b7359439f359105e.png",
+          "size": "medium"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/174s/2bf5dc4212d445d0b7359439f359105e.png",
+          "size": "large"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/300x300/2bf5dc4212d445d0b7359439f359105e.png",
+          "size": "extralarge"
+        }, {
+          "#text": "http://img2-ak.lst.fm/i/u/2bf5dc4212d445d0b7359439f359105e.png",
+          "size": "mega"
+        }]
+      }, {
+        "name": "Louis Gaston presents Digital Offspring",
+        "listeners": "191",
+        "mbid": "91b8b06a-d28a-46e6-a1ba-57faaf7743bc",
+        "url": "http://www.last.fm/music/Louis+Gaston+presents+Digital+Offspring",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }, {
+        "name": "zatknись (Сатана Печет Блины punk cover)похож на: stone sour foo fighters северный флот 3 Doors Down rasmus korn lumen слот louna rise against Finger Eleven Placebo blink 182 offspring Papa Roach the amatory Godsmack nickelback Король и Шут КИШ кукрыниксы",
+        "listeners": "526",
+        "mbid": "",
+        "url": "http://www.last.fm/music/zatkn%D0%B8%D1%81%D1%8C+(%D0%A1%D0%B0%D1%82%D0%B0%D0%BD%D0%B0+%D0%9F%D0%B5%D1%87%D0%B5%D1%82+%D0%91%D0%BB%D0%B8%D0%BD%D1%8B+punk+cover)%D0%BF%D0%BE%D1%85%D0%BE%D0%B6+%D0%BD%D0%B0:+stone+sour+foo+fighters+%D1%81%D0%B5%D0%B2%D0%B5%D1%80%D0%BD%D1%8B%D0%B9+%D1%84%D0%BB%D0%BE%D1%82+3+Doors+Down+rasmus+korn+lumen+%D1%81%D0%BB%D0%BE%D1%82+louna+rise+against+Finger+Eleven+Placebo+blink+182+offspring+Papa+Roach+the+amatory+Godsmack+nickelback+%D0%9A%D0%BE%D1%80%D0%BE%D0%BB%D1%8C+%D0%B8+%D0%A8%D1%83%D1%82+%D0%9A%D0%98%D0%A8+%D0%BA%D1%83%D0%BA%D1%80%D1%8B%D0%BD%D0%B8%D0%BA%D1%81%D1%8B",
+        "streamable": "0",
+        "image": [{"#text": "", "size": "small"}, {
+          "#text": "",
+          "size": "medium"
+        }, {"#text": "", "size": "large"}, {
+          "#text": "",
+          "size": "extralarge"
+        }, {"#text": "", "size": "mega"}]
+      }]
+    },
+    "@attr": {"for": "offspring"}
+  }
+};
+/* eslint-enable */
+
 describe('API', function() {
+  describe('search.artist()', function() {});
 
+  describe('search.artist.successHandler()', function() {
+    var noop = function() {};
+    var results = search.artist.successHandler(data, noop);
+
+    it('should return an Object which has a `query`' +
+      ' and `matches` property keys', function() {
+      expect(results)
+        .to.be.an('Object');
+
+      expect(results)
+        .to.have.property('query')
+        .that.is.a('String');
+
+      expect(results)
+        .to.have.property('matches')
+        .that.is.a('Array');
+    });
+
+    it('should return no matches with an empty `mbid` property', function() {
+      results.matches.forEach(function(result) {
+        expect(result.mbid)
+          .to.be.a('String')
+          .and.to.not.be.empty;
+      });
+    });
+  });
+
+  describe('search.artist.errorHandler()', function() {});
+
+  describe('search.artist.formatData()', function() {
+    it('should return an Object with a `mbid` and `name`' +
+      ' property key', function() {
+      // The Offspring
+      var artist = data.results.artistmatches.artist[0];
+      var results = search.artist.formatData(artist);
+
+      expect(results)
+        .to.be.an('Object')
+        .and.to.include.keys('mbid', 'name');
+    });
+
+    it('should return `thumbnail` object if images exists', function() {
+      // The Offspring
+      var artist = data.results.artistmatches.artist[0];
+      var thumbnail1x = artist.image[0]['#text'];
+      var thumbnail2x = artist.image[1]['#text'];
+      var results = search.artist.formatData(artist);
+
+      expect(results)
+        .to.have.deep.property('thumbnail.1x')
+        .to.equal(thumbnail1x)
+        .and.to.be.a('String');
+
+      expect(results)
+        .to.have.deep.property('thumbnail.2x')
+        .to.equal(thumbnail2x)
+        .and.to.be.a('String');
+    });
+
+    it('should not return `thumbnail` object if images dont exist', function() {
+      // Celtic Offspring
+      var artist = data.results.artistmatches.artist[6];
+      var results = search.artist.formatData(artist);
+
+      expect(results)
+        .to.not.have.deep.property('thumbnail');
+    });
+  });
 });
-
