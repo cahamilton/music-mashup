e2fbd9be3b74ce24f5e1dce7f89199e437dd5b3f
Carl Hamilton <carlanthonyhamilton@gmail.com>
2017-06-05 22:09:51 +1000

    Add info endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const search = require('./api/search');
+const info = require('./api/info');
 
 const router = express.Router();
 
@@ -9,6 +10,16 @@ router.get('/search/', (req, res, next) => {
 
 router.get('/search/:artistName', (req, res, next) => {
   search.artist(req.params.artistName)
+    .then(results => res.json(results))
+    .catch(error => res.status(500).json(error));
+});
+
+router.get('/info/', (req, res, next) => {
+  res.status(404).json({ error: 'Missing musicBrainzID parameter' });
+});
+
+router.get('/info/:musicBrainzID', (req, res, next) => {
+  info.mbid(req.params.musicBrainzID)
     .then(results => res.json(results))
     .catch(error => res.status(500).json(error));
 });
---
+++ routes/api/info.js
@@ -0,0 +1,40 @@
+const config = require('../../config');
+const LastFmNode = require('lastfm').LastFmNode;
+
+const lastfm = new LastFmNode({
+  api_key: config.keys.lastFM.key,
+  secret: config.keys.lastFM.secret,
+});
+
+const info = {};
+
+/**
+ * Function to query LastFM API for artist matches
+ * @param {String} musicBrainzID - Artist to search for
+ */
+info.mbid = musicBrainzID => new Promise((resolve, reject) => {
+  lastfm.request('artist.getInfo', {
+    mbid: musicBrainzID,
+    handlers: {
+      success: results => resolve(info.mbid.successHandler(results)),
+      error: error => reject(info.mbid.errorHandler(error)),
+    },
+  });
+});
+
+/**
+ * Success handler for info.mbid() function
+ * @param {Object} data - Object of data from LastFM API call
+ * @return {Object} results - Newly formatted results object
+ */
+
+info.mbid.successHandler = data => data;
+
+/**
+ * Error handler for info.mbid() function
+ * @param {Object} error - Object of error data from LastFM API call
+ * @return {Object} error - Object of error data
+ */
+info.mbid.errorHandler = error => ({ error: error.message });
+
+module.exports = info;
