6bd6af04e278f679bb499535716ed0d73750c0e0
Carl Hamilton <carlanthonyhamilton@gmail.com>
2018-03-12 15:42:35 +1100

    Add snapshot test for Logo

--- assets/scripts/components/Header/Logo/Logo.jsx
+++ assets/scripts/components/Header/Logo/Logo.jsx
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import logo from './logo.svg';
 import styles from './Logo.pcss';
 
-class Logo extends Component {
-  render() {
-    return (
-      <div className={styles.container}>
-        <img src={logo} width="200" height="123" alt="Music Mashup" title="Music Mashup" />
-      </div>
-    );
-  }
-}
+const Logo = () => (
+  <div className={styles.container}>
+    <img src={logo} width="200" height="123" alt="Music Mashup" title="Music Mashup" />
+  </div>
+);
 
 export default Logo;
