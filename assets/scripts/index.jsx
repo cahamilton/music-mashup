57d6344caa54feb868fbb89fa067e34d102b008d
Carl Hamilton <carlanthonyhamilton@gmail.com>
2018-12-24 23:32:36 +1100

    Update redux store configuration

--- assets/scripts/index.jsx
+++ assets/scripts/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import reducers from './reducers/reducers';
+
+const enhancer = compose(
+  applyMiddleware(thunk),
+  devToolsEnhancer(),
+);
 
 const store = createStore(
-  devToolsEnhancer(),
+  reducers,
+  enhancer,
 );
 
 const RootContainer = document.getElementById('musicMashup');
