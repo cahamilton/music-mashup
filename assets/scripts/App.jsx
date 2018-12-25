498d51ce1b28a5e4944ca8965e30577ef495d0a1
Carl Hamilton <carlanthonyhamilton@gmail.com>
2018-12-25 23:26:12 +1100

    Import Alternative component to main container

--- assets/scripts/App.jsx
+++ assets/scripts/App.jsx
@@ -5,10 +5,12 @@ import '../stylesheets/base/font-face.pcss';
 import '../stylesheets/base/global.pcss';
 
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 const App = () => (
   <div className="container">
     <Header />
+    <Main />
   </div>
 );
 
