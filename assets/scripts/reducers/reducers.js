6db17f50d324104b48d500afe966759f126d89ea
Carl Hamilton <carlanthonyhamilton@gmail.com>
2018-12-25 22:45:58 +1100

    Add info to root reducers

--- assets/scripts/reducers/reducers.js
+++ assets/scripts/reducers/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import info from './info/info.reducers';
 import search from './search/search.reducers';
 
 export default combineReducers({
+  info,
   search,
 });
