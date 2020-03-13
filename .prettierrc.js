b161c3d090fca81410e80e987a1b430cd67f5a6e
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-03-13 14:16:48 +0000

    Add Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+/** @format */
+
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  parser: 'babel',
+  requirePragma: false,
+  insertPragma: true,
+  endOfLine: 'lf',
+};
