cc961e6fe6baeaba5cb2d5d161e2e923a4445f09
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-03-23 11:05:55 +0000

    Add getWikipediaTitle

---
+++ routes/api/biography/getWikipediaTitle.js
@@ -0,0 +1,37 @@
+/** @format */
+
+const { get } = require('axios');
+
+const logger = require('../../../logger');
+
+/**
+ * Return Wikipedia title from a Wikidata ID
+ * @param {string} wikidataId - Wikidata ID
+ * @return {Promise<string|null>}
+ */
+const getWikipediaTitle = async (wikidataId) => {
+  if (!wikidataId) {
+    return null;
+  }
+
+  try {
+    const response = await get('https://www.wikidata.org/w/api.php', {
+      params: {
+        action: 'wbgetentities',
+        format: 'json',
+        ids: wikidataId,
+        props: 'sitelinks',
+      },
+    });
+
+    const { title } = response.data.entities[wikidataId].sitelinks.enwiki;
+
+    return title;
+  } catch (error) {
+    logger.error(error.message);
+
+    return null;
+  }
+};
+
+module.exports = getWikipediaTitle;
