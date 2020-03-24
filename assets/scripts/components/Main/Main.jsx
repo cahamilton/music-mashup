e16dd5a02a313b8e3a78840e487b28e41ac70778
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-03-24 11:43:02 +0000

    Import ArtistHeader to Main component

--- assets/scripts/components/Main/Main.jsx
+++ assets/scripts/components/Main/Main.jsx
@@ -1,12 +1,16 @@
+/** @format */
+
 import React from 'react';
 
 import styles from './Main.pcss';
 
 import Alternative from './Alternative/container/Alternative';
+import ArtistHeader from './Artist/container/ArtistHeader';
 
 const Main = () => (
   <main className={styles.container} role="main">
     <Alternative />
+    <ArtistHeader />
   </main>
 );
 
