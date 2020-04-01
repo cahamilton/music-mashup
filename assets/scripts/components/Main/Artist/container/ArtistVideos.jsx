e92b428037471cafdf03416c3ab2e6b3fac910bd
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-04-01 20:56:42 +0100

    Add ArtistVideos component

---
+++ assets/scripts/components/Main/Artist/container/ArtistVideos.jsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import { connect } from 'react-redux';
+
+import Component from '../ArtistVideos';
+import { videosSearch } from '../../../../actions/videos/videos.actions';
+
+const mapStateToProps = (state) => {
+  const { relationYoutube } = state.info;
+  const { loading, results } = state.videos;
+
+  return {
+    isLoading: loading,
+    source: relationYoutube,
+    videos: results.items,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  onSourceUpdate: () => dispatch(videosSearch()),
+});
+
+const ArtistVideos = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export default ArtistVideos;
--- assets/scripts/components/Main/Main.jsx
+++ assets/scripts/components/Main/Main.jsx
@@ -7,12 +7,14 @@ import styles from './Main.pcss';
 import Alternative from './Alternative/container/Alternative';
 import ArtistBiography from './Artist/container/ArtistBiography';
 import ArtistHeader from './Artist/container/ArtistHeader';
+import ArtistVideos from './Artist/container/ArtistVideos';
 
 const Main = () => (
   <main className={styles.container} role="main">
     <Alternative />
     <ArtistHeader />
     <ArtistBiography />
+    <ArtistVideos />
   </main>
 );
 
