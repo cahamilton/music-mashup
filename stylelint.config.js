c4f4b44f861a6b31874306732e8692cff267dc17
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-04-09 17:29:38 +0100

    Add and configure Stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+/** @format */
+
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-property-sort-order-smacss',
+  ],
+};
